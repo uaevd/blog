a2c20a2da3e7ce0b32b63b9558087b2de53f5e57
uaevd <saekkigae45@gmail.com>
2023-09-17 10:51:40 +0700

    Create RegisterBlogDialog

---
+++ src/components/organisms/RegisterBlogDialog/index.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { RegisterBlogDialog } from '.';
+
+export default {
+    title: 'Organisms/RegisterBlogDialog',
+    component: RegisterBlogDialog,
+} as Meta<typeof RegisterBlogDialog>;
+
+type Story = StoryObj<typeof RegisterBlogDialog>;
+
+export const Default: Story = {};
